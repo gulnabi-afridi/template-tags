a8f43692feb938e4a848b2fd68cbb41ee5b7d65d
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-06-24 17:34:53 +0500

    layout is set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./views/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "my-background-image": "url('/path/to/your/background-image.jpg')",
+      },
+    },
+    screens: {
+      xs: "325px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1280px",
+    },
+    colors: {
+      black: {
+        main: "#000000",
+        off: " #434343",
+        cool: "#1f1f1f",
+      },
+      white: {
+        main: "#FFFFFF",
+        off: "#dbdbdb",
+      },
+      blue: {
+        main: "#007de3",
       },
     },
   },
   plugins: [],
-}
+};
+
+// #434343
